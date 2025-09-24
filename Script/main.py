383c13a28903694fdb1722a1ae3c8ccdb32300ed
A89881 <91288859+A89881@users.noreply.github.com>
2025-09-24 13:16:24 +0200

    BaFe2As2 - Run

--- Script/main.py
+++ Script/main.py
@@ -16,7 +16,7 @@ import os
 # (Please copy and paste it into the top and align it correctly)
 # i    j    dx   dy   dz   Jij         χ⁰↑             χ⁰↓
 
-url = r"NM-Cr\NM-Cr-U2\NM-chfile-1.dat"    
+url = r"BaFe2As2\No_Dope\BaFe2As2-nodope.dat"    
 base_folder = os.path.dirname(url)
 
 # === PHYSICAL PARAMETERS ===
@@ -28,47 +28,70 @@ radius = 5  # Cutoff radius for valid k-site neighbors
 # Input format: (i, j): (dx, dy, dz) shifting vector'
 
 shift_rules_null = {}
-shift_rules_AFM = {
+shift_rules_AFM_Cr = {
     (1, 2): (-0.5, -0.5, -0.5),
     (2, 1): (0.5, 0.5, 0.5),
 }
-shift_rules_TaSe = {
+shift_rules_AFM_TaSe = {
     (1, 2): (0.0, 0.0, 0.5),
     (2, 1): (0.0, 0.0, -0.5)
 }
+shift_rules_BaFe2As2 = {
+    (3, 4): (0.0, 0.0, 0.5),
+    (4, 3): (0.0, 0.0, -0.5),
+
+    (3, 5): (0.5, 0.5, 0.0),
+    (5, 3): (-0.5, -0.5, 0.0),
+
+    (3, 6): (0.5, 0.5, 0.5),
+    (6, 3): (-0.5, -0.5, -0.5),
+
+    (4, 5): (0.5, -0.5, 0.0),
+    (5, 4): (-0.5, 0.5, 0.0),
+
+    (4, 6): (0.5, -0.5, 0.5),
+    (6, 4): (-0.5, 0.5, -0.5),
+
+    (5, 6): (0.0, 0.0, 0.5),
+    (6, 5): (0.0, 0.0, -0.5),
+}
+base_change_matrix_BaFe2As2 = [
+    [1.0, 0.0, 0.0],
+    [0.0, 1.0, 0.0],
+    [0.0, 0.0, 3.2849967479]
+]
 # === TRANSFORMATION MATRIX (depends on lattice symmetry and geometry) ===
-base_change_matrix_NM = [
+base_change_matrix_NM_Cr = [
     [1.0, 0.0, 0.0],
     [-0.333333333333333, 0.942809033637852, 0.0],
     [-0.333333333333333, -0.471404564484194, 0.816496546528280]
 ]
-base_change_matrix_AFM = [
+base_change_matrix_AFM_Cr = [
     [1.0, 0.0, 0.0],
     [0.0, 1.0, 0.0],
     [0.0, 0.0, 1.0]
 ]
-base_change_matrix_BCC = [
+base_change_matrix_BCC_Fe = [
     [-0.5, 0.5, 0.5],
     [0.5, -0.5, 0.5],
     [0.5, 0.5, -0.5]
 ]
-base_change_matrix_FCC = [
-    [-0.5, 0.0, 0.5],
-    [0.0, 0.5, 0.5],
-    [-0.5, 0.5, 0.0]
-]
-base_change_matrix_TaSe = [
+base_change_matrix_AFM_TaSe = [
     [0.866025403784439, -0.5, 0.0],
     [0.0, 1.0, 0.0],
     [0.0, 0.0, 3.85960152170524]
 ]
-
+base_change_matrix_NM_Cu_FCC = [
+    [0.5, 0.5, 0.0],
+    [0.5, 0.0, 0.5],
+    [0.0, 0.5, 0.5]
+]
 # === STEP 1: Data Formatting with Optional Sublattice Shifting ===
 time_start = t.time()
 f_url = format_data(
     url, 
     output_file=os.path.join(base_folder, "formated_data.csv"),
-    shift_map=shift_rules_null,
+    shift_map=shift_rules_BaFe2As2,
     scale_response=1.0 # Default is 1.0
 )
 
@@ -76,7 +99,7 @@ f_url = format_data(
 k_suit_url = det_K_suit(
     f_url, 
     radius, 
-    base_change_matrix_NM, 
+    base_change_matrix_BaFe2As2, 
     output_file=os.path.join(base_folder, "k_pot_coords.csv")
 )
 
@@ -112,7 +135,7 @@ print(f"[main.py] Data preparation and χ^zz computation done. Runtime: {time_en
 # plot_static_and_spin_decay(
 #     static_file=f_url,
 #     spin_file=X_file,
-#     transform_matrix=base_change_matrix_AFM,
+#     transform_matrix=base_change_matrix_AFM_Cr,
 #     scale_diagonal=[Length_scale] * 3,
 #     output_static=os.path.join(base_folder, "static_decay_plot.png"),
 #     output_xzz=os.path.join(base_folder, "xzz_decay_plot.png"),
@@ -131,17 +154,30 @@ print(f"[main.py] Data preparation and χ^zz computation done. Runtime: {time_en
 #     output_comparison=os.path.join(base_folder, "comparison_decay_plot.png")
 # )
 
-# NM-Cr Setup (enabled by default)
-Length_scale = 4.640997226251
+# # NM-Cr Setup (enabled by default)
+# Length_scale = 4.640997226251
+# plot_static_and_spin_decay(
+#     static_file=f_url,
+#     spin_file=X_file,
+#     transform_matrix=base_change_matrix_NM_Cr,
+#     scale_diagonal=[Length_scale] * 3,
+#     output_static=os.path.join(base_folder, "static_decay_plot.png"),
+#     output_xzz=os.path.join(base_folder, "xzz_decay_plot.png"),
+#     output_comparison=os.path.join(base_folder, "comparison_decay_plot.png")
+# )
+
+# === Plotting setup for BaFe2As2 ===
+Length_scale_BaFe2As2 = 7.4880429335  
 plot_static_and_spin_decay(
     static_file=f_url,
     spin_file=X_file,
-    transform_matrix=base_change_matrix_NM,
-    scale_diagonal=[Length_scale] * 3,
-    output_static=os.path.join(base_folder, "static_decay_plot.png"),
-    output_xzz=os.path.join(base_folder, "xzz_decay_plot.png"),
-    output_comparison=os.path.join(base_folder, "comparison_decay_plot.png")
+    transform_matrix=base_change_matrix_BaFe2As2,
+    scale_diagonal=[Length_scale_BaFe2As2] * 3,
+    output_static=os.path.join(base_folder, "BaFe2As2_static_decay_plot.png"),
+    output_xzz=os.path.join(base_folder, "BaFe2As2_xzz_decay_plot.png"),
+    output_comparison=os.path.join(base_folder, "BaFe2As2_comparison_decay_plot.png")
 )
+
 
 # # TaSe2 Setup (enabled by default)
 # Length_scale = 6.760364108039488
